eca0fffa199117da4f72c9976956ce20e2586559
Devaofficial-06 <devakrishnaofficial06@gmail.com>
2026-02-07 12:38:20 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/deva_animation_portfolio',
+};
+
+export default nextConfig;
